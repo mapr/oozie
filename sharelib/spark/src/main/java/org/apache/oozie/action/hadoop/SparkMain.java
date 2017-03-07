99a99ae39ab43092fff1fb79cd011f6d47153b9f
Sergey Svynarchuk <ssvinarchuk@cybervisiontech.com>
2017-03-07 16:04:01 +0200

    Fix Oozie buid

--- sharelib/spark/src/main/java/org/apache/oozie/action/hadoop/SparkMain.java
+++ sharelib/spark/src/main/java/org/apache/oozie/action/hadoop/SparkMain.java
@@ -29,6 +29,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
