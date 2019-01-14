3d42103c3f94c91b6f76064bd5ca543a31066574
Sergey Svynarchuk <ssvinarchuk@mapr.com>
2019-01-14 16:31:56 +0200

    Revert "MAPR-OOZIE-100: Oozie workflow fails with an exception "java.io.FileNotFoundException: hive-site.xml (Permission denied)""
    This reverts commit 0b377ef9a6a9e8d82df973881d8adfe1c2b58cb4.

--- sharelib/spark/src/main/java/org/apache/oozie/action/hadoop/SparkMain.java
+++ sharelib/spark/src/main/java/org/apache/oozie/action/hadoop/SparkMain.java
@@ -541,14 +541,6 @@ public class SparkMain extends LauncherMain {
      * @throws IOException
      */
     private void setHiveSite(Configuration hiveConf) throws IOException {
-        File hiveFile = new File(HIVE_SITE_CONF);
-        if (hiveFile.exists()) {
-            System.out.println("Hive-site.xml will be overwritten in container.");
-            Configuration conf = new Configuration();
-            conf.addResource(HIVE_SITE_CONF);
-            hiveConf.addResource(conf);
-            hiveFile.delete();
-        }
         // See https://issues.apache.org/jira/browse/HIVE-1411
         hiveConf.set("datanucleus.plugin.pluginRegistryBundleCheck", "LOG");
 
