8c014264fed64de946a796d0c6411ca1ced84006
Sergey Svynarchuk <sergey.svinarchuk@hpe.com>
2021-10-11 13:17:10 +0300

    OOZ-282: Added bc jars from cluster for ATS initialization

--- distro/src/main/bin/configure.sh
+++ distro/src/main/bin/configure.sh
@@ -152,6 +152,8 @@ copyMaprLibs() {
   findAndCopyJar "${MAPR_HOME}/lib" "${JETTY_LIB_DIR}" -iname "zookeeper-jute-[0-9].*.jar" 2> /dev/null
   findAndCopyJar "${MAPR_HOME}/lib" "${JETTY_LIB_DIR}" -iname "maprfs-[0-9].*jar" -not -name "*test*.jar" 2> /dev/null
   findAndCopyJar "${MAPR_HOME}/lib" "${JETTY_LIB_DIR}" -iname "mapr-security-web*.jar" -not -name "*test*.jar" 2> /dev/null
+  findAndCopyJar "${MAPR_HOME}/" "${JETTY_LIB_DIR}" -iname "bctls-fips*.jar" -not -name "*test*.jar" 2> /dev/null
+  findAndCopyJar "${MAPR_HOME}/" "${JETTY_LIB_DIR}" -iname "bc-fips*.jar" -not -name "*test*.jar" 2> /dev/null
 
   ln -sf ${MAPR_HOME}/lib/slf4j-log4j12-*  ${JETTY_LIB_DIR}
 }
